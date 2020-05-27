2a256e6ee978845946e5e66faeeb7607482fbe10
Zach Stein <51035066+Synzack@users.noreply.github.com>
2020-05-27 07:48:31 -0400

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Weaponizing 28 Year Old Xlm Macros",
+        "excerpt":"Overview Excel 4.0 macros (XLM macros) are a feature of Microsoft Excel that date back to 1992. These macros can be embedded into an Excel sheet and do not use VBA. This is significant as many modern exploit campaigns utilize VBA (Visual Basic for Applications) macros, which many security appliances...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Weaponizing-28-Year-Old-XLM-Macros/",
+        "teaser": null
+      }]
